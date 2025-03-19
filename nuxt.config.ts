b60c2153a15a2431d01f504f0107fa8a50b6dc99
sheymantech <olaniyansheyi1704@gmail.com>
2025-03-19 10:09:32 +0100

    new feature

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
-    },
-    private: {
       supabaseKey: process.env.SUPABASE_KEY,
-      openaiApiKey: process.env.OPENAI_API_KEY,
     },
   },
   plugins: [
